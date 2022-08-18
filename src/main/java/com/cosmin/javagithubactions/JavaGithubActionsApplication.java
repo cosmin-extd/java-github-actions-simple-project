23dece0521009c51c84fd8e1f396d241ebcb3182
Cosmin Dinu <cosmin.dinu.ext@extendaretail.com>
2022-08-18 08:30:57 +0300

    Spring Boot & Maven project files.

---
+++ src/main/java/com/cosmin/javagithubactions/JavaGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.cosmin.javagithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGithubActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cosmin/javagithubactions/JavaGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmin.javagithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
